1de118babaf81fc37c2ff86e283c91bc8b909dab
irinapr25 <88492720+irinapr25@users.noreply.github.com>
2025-01-09 19:58:38 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -140,7 +140,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 	//=======================================================
 	/* LANGUAGE CHANGE */
-	//languageChange();
+	languageChange();
 	//=======================================================
 
 	/* SPOILERS */
